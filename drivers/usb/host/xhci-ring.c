e91c2792911b235667789dd0181501d6d1c055d8
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2021-09-17 11:39:38 +0530

    Revert "FROMLIST: usb: host: add xhci hooks for USB offload"
    This reverts commit ed626b01e56be9be91ef8dc02a64bb0856e4e20d.
    
    Revert usb offload support to address boot issue seen in user
    variant.
    
    Tracked-On: OAM-99205
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -3009,15 +3009,6 @@ void xhci_update_erst_dequeue(struct xhci_hcd *xhci,
 }
 EXPORT_SYMBOL_GPL(xhci_update_erst_dequeue);
 
-static irqreturn_t xhci_vendor_queue_irq_work(struct xhci_hcd *xhci)
-{
-	struct xhci_vendor_ops *ops = xhci_vendor_get_ops(xhci);
-
-	if (ops && ops->queue_irq_work)
-		return ops->queue_irq_work(xhci);
-	return IRQ_NONE;
-}
-
 /*
  * xHCI spec says we can get an interrupt, and if the HC has an error condition,
  * we might get bad data out of the event ring.  Section 4.10.2.7 has a list of
@@ -3051,10 +3042,6 @@ irqreturn_t xhci_irq(struct usb_hcd *hcd)
 		ret = IRQ_HANDLED;
 		goto out;
 	}
-
-	ret = xhci_vendor_queue_irq_work(xhci);
-	if (ret == IRQ_HANDLED)
-		goto out;
 
 	/*
 	 * Clear the op reg interrupt status first,
